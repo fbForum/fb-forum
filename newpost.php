5a508caf6750eed555fe0d67f8df80ac3e29ff64
Pixoria <pixoria@users.noreply.github.com>
2015-12-11 18:10:27 +0800

    Create new posts
    newport.php creates a new post to the database server

---
+++ newpost.php
@@ -0,0 +1,43 @@
+
+<html>
+<head>
+	<title>發表新文章</title>
+	<style type="text/css">
+	</style>
+
+</head>
+<body>
+
+
+
+<table align="center" border="1" bordercolor="#ccc" cellpadding="5" cellspacing="0" style="border-collapse:collapse;" width="800">
+	<thead>
+		<tr>
+			<th bgcolor="#275187" scope="col"><span style="color:#FFFFE0;"><span style="font-family:verdana,geneva,sans-serif;">發表貼文</span></span></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+			<form  method="POST" action="dbc.php">
+				<p><input maxlength="12" name="author" size="100" type="text" value="作者" /></p>
+        <p><input maxlength="255" name="topic" size="100" type="text" value="輸入標題" /></p>
+			<p><textarea width="100%" cols="1" name="content" rows="1"></textarea></p>
+
+			<p><input name="submit" type="submit" value="發表" /><input name="reset" type="reset" value="Reset" /></p>
+
+			<p>&nbsp;</p>
+			</form>
+			</td>
+		</tr>
+		<tr>
+
+		</tr>
+	</tbody>
+</table>
+
+
+
+<p>&nbsp;</p>
+</body>
+</html>
