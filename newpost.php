b5d86dc075852bda573696f9d8835da118c21e82
Pixoria <pixoria@Mac-mini.local>
2015-12-12 23:44:28 +0800

    New thread page
    bug fixed.

--- newpost.php
+++ newpost.php
@@ -10,7 +10,7 @@
 
 
 
-<table align="center" border="1" bordercolor="#ccc" cellpadding="5" cellspacing="0" style="border-collapse:collapse;" width="800">
+<table align="center" border="1" bordercolor="#ccc" cellpadding="5" cellspacing="0" style="border-collapse:collapse;" width=100%>
 	<thead>
 		<tr>
 			<th bgcolor="#275187" scope="col"><span style="color:#FFFFE0;"><span style="font-family:verdana,geneva,sans-serif;">發表貼文</span></span></th>
@@ -20,9 +20,9 @@
 		<tr>
 			<td>
 			<form  method="POST" action="dbc.php">
-				<p><input maxlength="12" name="author" size="100" type="text" value="作者" /></p>
-        <p><input maxlength="255" name="topic" size="100" type="text" value="輸入標題" /></p>
-			<p><textarea width="100%" cols="1" name="content" rows="1"></textarea></p>
+				<p><input maxlength="12" name="author" size=100% type="text" value="作者" /></p>
+        <p><input maxlength="255" name="topic" size=100% type="text" value="輸入標題" /></p>
+			<p><textarea width="100%" cols="100" name="content" rows="10"></textarea></p>
 
 			<p><input name="submit" type="submit" value="發表" /><input name="reset" type="reset" value="Reset" /></p>
 
