1fed6219d3af5e54c2d978316744e432d5008fd3
Pixoria <pixoria@Mac-mini.local>
2015-12-15 16:41:28 +0800

    Newpost.php
    Implementation of SCEditor in the newport form

--- newpost.php
+++ newpost.php
@@ -5,7 +5,9 @@
 	<title>發表新文章</title>
 	<style type="text/css">
 	</style>
-
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="minified/themes/default.min.css" type="text/css" media="all" />
+	<script type="text/javascript" src="minified/jquery.sceditor.bbcode.min.js"></script>
 </head>
 <body>
 
@@ -20,16 +22,25 @@
 	<tbody>
 		<tr>
 			<td>
-			<form  method="POST" action="dbc.php">
-				<p><input maxlength="12" name="author" size=100% type="text" value="作者" /></p>
-				<select name="type"><option value="BW">吹水台</option><option value="CA">時事台</option><option value="ET">娛樂台</option><option value="EP">創意台</option><option value="MU">音樂台</option><option value="DC">攝影台</option><option value="ED">飲食台</option></select>
-        <p><input maxlength="255" name="topic" size=100% type="text" value="輸入標題" /></p>
-				<p><textarea width="100%" cols="75" name="content" rows="5"></textarea></p>
+				<form  method="POST" action="dbc.php">
+					<p><input maxlength="12" name="author" size="100" type="text" value="作者" /></p>
+	<select name="type"><option value="BW">吹水台</option><option value="CA">時事台</option><option value="ET">娛樂台</option><option value="EP">創意台</option><option value="MU">音樂台</option><option value="DC">攝影台</option><option value="ED">飲食台</option></select>
+	        <p><input maxlength="255" name="topic" size="100" type="text" value="輸入標題" /></p>
+					<p><textarea width="100%" cols="100" name="content" rows="20"></textarea></p>
+					<script>
+					$(function() {
+					    // Replace all textarea's
+					    // with SCEditor
+					    $("textarea").sceditor({
+					        plugins: "bbcode",
+						style: "/minified/jquery.sceditor.default.min.css"
+					    });
+					});
+					</script>
+				<p><input name="submit" type="submit" value="發表" /><input name="reset" type="reset" value="Reset" /></p>
 
-			<p><input name="submit" type="submit" value="發表" /><input name="reset" type="reset" value="Reset" /></p>
-
-			<p>&nbsp;</p>
-			</form>
+				<p>&nbsp;</p>
+				</form>
 			</td>
 		</tr>
 		<tr>
